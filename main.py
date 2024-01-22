08367f0bbfdde5792e2a3038ade0c251a0dab58f
Mimi Reyburn <mimireyburn@gmail.com>
2024-01-22 17:22:43 +0000

    Bug fix GPIO

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from pk import take_photo
 import RPi.GPIO as GPIO
+import time
 
 if __name__ == "__main__":
     # Set up GPIO
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+    print("Waiting for button press...")
+
     # Take a photo when the button is pressed
     while True:
-        input_state = GPIO.input(18)
+        input_state = GPIO.input(17)
         if input_state == False:
-            take_photo(path)
+            take_photo()
             time.sleep(0.2)
+            print("Photo taken!")
